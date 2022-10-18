37713f7c0f48928affedc2777325311e8c100e86
FrancescoRizzi00 <rizzi.francesco@panettipitagora.edu.it>
2022-10-18 20:04:11 +0200

    Back end non funzionante
    Infarinatura del back end ma non funzionante

--- Stipend.php
+++ Stipend.php
@@ -2,13 +2,13 @@
     <div style='margin-top:15px; padding:5px; background-color:white'>
 >
 <form action='<?php echo $_SERVER["PHP_SELF"];?>' method="post">
-		<input type="text" name="glavoro" placeholder="Quanti giorni durera' il lavoro?"/><br/>
+		<input type="number" name="glavoro" placeholder="Quanti giorni durera' il lavoro?"/><br/>
         <p size="4", color="red">Campo obbligatorio</p><br>
         <p>Ore giornaliere di lavoro</p><br>
-		<input type="text" name="hlavoro" placeholder="Quante ore lavori al giorno?"/><br/>
+		<input type="number" name="hlavoro" placeholder="Quante ore lavori al giorno?"/><br/>
         <p size="4", color="red">Campo obbligatorio</p><br>
         <p>Compenso orario</p>
-        <input type="text" name="poraria" placeholder="Quanto verrai pagato all'ora?"/><br>
+        <input type="number" name="poraria" placeholder="Quanto verrai pagato all'ora?"/><br>
         <p size="4", color="red">Campo obbligatorio</p><br>
 		<input type="submit" name="calcolo" value="Calcola"/><br>
         <p>Guadagnerai <b>300$</b>lavorando 3 giorni al compenso di 30$/h</p>
@@ -16,17 +16,29 @@
 </div>
 </html>
 <?php
-if (isset($_POST["tasto_invia"]))
+if (isset($_POST["calcolo"]))
 {
     $glav = $_POST["glavoro"];
     $hlav = $_POST["hlavoro"];
     $pora = $_POST["poraria"];
-    $paga = 0; 
 
-    $paga = $paga ($pora*$hlav)*$glav; 
+    $n1 = $glav; 
+    $n2 = $hlav; 
+    $n3 = $pora; 
 
+    echo $n1; 
+    echo $n2; 
+    echo $n3; 
+
+    function tot($n1, $n2, $n3){
+        
+        $tot = [($n3*$n2)*$n1]; 
+        return $tot; 
+    }
+    $tot = tot($n1, $n2, $n3); 
+    
+    echo $tot; 
     echo "la paga per questo lavoro ti renderà : "; 
-    echo $paga; 
     echo "$";
 
 }
